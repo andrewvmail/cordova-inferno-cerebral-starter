c550e0a4d77a61c36ce622e0897a5249983c8c1c
momoterraw <andrewvmail@gmail.com>
2019-01-16 01:57:24 -0800

    add cerebral

--- inferno-app/src/index.js
+++ inferno-app/src/index.js
@@ -1,17 +1,31 @@
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import React from  'react';
-import ReactDOM from  'react-dom';
-import Framework7 from 'framework7/framework7.esm.bundle.js';
-import Framework7React from 'framework7-react';
+import * as serviceWorker from "./serviceWorker";
+
+import ReactDOM from "inferno-compat";
+import Framework7 from "framework7/framework7.esm.bundle.js";
+import Framework7React from "framework7-react";
+import "./index.css";
+
+import App from "cerebral";
+import AppComponent from "./App";
+import Devtools from "cerebral/devtools";
+import { Container } from "@cerebral/inferno";
+
+import main from "./main";
+
+const app = App(main, {
+  devtools: Devtools({ host: "localhost:7777" })
+});
 
 Framework7.use(Framework7React);
 
 ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-)
+  <Container app={app}>
+    <AppComponent />
+  </Container>,
+  document.getElementById("root")
+);
+
+
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
